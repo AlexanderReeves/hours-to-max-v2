23cd05e8dd2e7a278b31fccc3e0fa0dab5993d23
AlexanderReeves <reevesdrums@gmail.com>
2024-11-01 15:05:24 +1100

    new version with bootstrap begin

---
+++ app.js
@@ -0,0 +1,46 @@
+//Server
+const express = require('express');
+//Mongoose for mongodb
+const mongoose = require('mongoose');
+//File pathing
+const path = require('path');
+
+//Env files
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env'});
+
+//App is the server
+const app = express();
+
+
+
+//No need for urlParse or UnifiedTopology args as they were removed and deprecated
+//Conect via location saved in env file
+const uri = process.env.DBURI;
+//mongoose.connect(uri)
+mongoose.connect('mongodb+srv://reevesalexanderj:nala1234@hours-to-max.4jrpf.mongodb.net/?retryWrites=true&w=majority&appName=hours-to-max')
+//.then can be used after async processes to do something after a promise is fulfilled.
+//Start listening if db connected
+    .then((result) => app.listen(3000) ,console.log("Connected to db!"), test())
+    .catch((err) => console.log(err))
+
+//__dirname is a variable that gives access to the current directory
+const publicDirectory = path.join(__dirname, './public')
+app.use(express.static(publicDirectory));
+
+//HTML viewing engine
+app.set('view engine', 'hbs');
+
+
+app.get("/", (req, res) => {
+    res.render('index')
+});
+
+
+app.get("/register", (req, res) => {
+    res.render('register')
+});
+
+function test(){
+    console.log("test");
+}
