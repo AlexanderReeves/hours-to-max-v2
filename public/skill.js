ae6afc022358e5983fc2644245b9a87b574f2899
AlexanderReeves <reevesdrums@gmail.com>
2025-01-30 17:33:34 +1100

    started a more in depth rewrite for scalability, default training method selection, cleared out old code

--- public/skill.js
+++ public/skill.js
@@ -1,21 +1,34 @@
 //The Skill Class
 class Skill {
   //a # can be used to privatise a variable
-  name = "";
-  currentXp = 0;
-  dropdownSelection = 0;
-  xpRates = [];
-  gpPerXpRates = []; 
+  name = ""; //The name of a skill
+  currentXp = 0; //The players current xp in that skill
+  dropdownSelection = 0; //The current skill training choice from the dropdown
+  xpRates = []; //The xp rates that each training method offers
+  gpPerXpRates = []; //The cost per xp for each training method
+
   
-  constructor(name, xpRates, gpPerXpRates, defaultSelection) {
+  constructor(name, xpRates, gpPerXpRates, defaultSelection, dropdownName) {
+    //Add the skill name
     this.name = name;
+    //Fill the array with available xp rates for the training methods in the dropdown
     this.xpRates = xpRates;
+    //Fill the array with the cost per xp for each of the training methods in dropdown
     this.gpPerXpRates = gpPerXpRates;
+    //Set the default training method for each skill
     this.dropdownSelection = defaultSelection;
+    //Identify the name of the corresponding html dropdown
+    this.dropdownName = dropdownName;
   }
 
   LogName(){
     console.log(this.name)
+  }
+
+  SelectDefaultTrainingMethod(){
+    //sets the corresponding dropdown to the default training method
+    //Each skill has a corresponding dropdown with name being skillnamedropdown
+    $('#' + this.name +'Dropdown :nth-child(4)').prop('selected', true);
   }
 
 }
