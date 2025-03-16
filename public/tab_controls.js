eb0ab1b91a56633ac697a91b39e1558fb030e279
AlexanderReeves <reevesdrums@gmail.com>
2025-03-16 14:42:51 +1100

    intro message change and cost per xp can be a double with joi validation

--- public/tab_controls.js
+++ public/tab_controls.js
@@ -23,7 +23,7 @@ function setTab(tabname) {
         if(tabname == "max"){
            themedElements[i].className = themedElements[i].className.replace("newtheme", "themered");
           document.getElementById('intro').innerText =
-           "Find how many hours until you Max in Old School Runescape.";
+           "How long does it take to Max in Old School Runescape?";
            document.getElementById('goalCapeDisplay').innerText = "Max Cape";
            document.getElementById("finalCape").classList.remove('finalQuest');       
            document.getElementById("finalCape").classList.remove('finalAchievement');       
