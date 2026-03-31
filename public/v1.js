c4180fe12b3b811c6804c3983c37019be3374c2c
AlexanderReeves <reevesdrums@gmail.com>
2026-03-31 11:46:37 +1100

    alt cors proxy test

--- public/v1.js
+++ public/v1.js
@@ -214,7 +214,9 @@ function PullFromJagex(){
     }
 
     //Attempt to pull player from the Jagex API
-    $.getJSON("https://corsproxy.io/?url=https://secure.runescape.com/m=hiscore_oldschool/index_lite.json?player=" + user, function(result) {
+    //$.getJSON("https://corsproxy.io/?url=https://secure.runescape.com/m=hiscore_oldschool/index_lite.json?player=" + user, function(result) {
+    
+    $.getJSON("https://proxy.corsfix.com/?url=https://secure.runescape.com/m=hiscore_oldschool/index_lite.json?player=" + user, function(result) {
         console.log(result);
         $.each(result, function(pulledkey, field) {
             console.log(field + "TEST");
